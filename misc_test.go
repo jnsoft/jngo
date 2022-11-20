75e0e12723afa4fa7f3c7c0816f956e109fb66cf
JN <jn@jnsoft.se>
2022-11-20 19:37:45 +0100

    misc_test

--- misc_test.go
+++ misc_test.go
@@ -1,6 +1,7 @@
 package misc
 
 import (
+	"strings"
 	"testing"
 
 	. "github.com/jnsoft/jngo/testhelper"
@@ -20,5 +21,36 @@ func TestReduce(t *testing.T) {
 		}
 
 		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(x int) bool {
+			return x%2 == 0
+		})
+		AssertTrue(t, found)
+		AssertEqual(t, firstEvenNumber, 4)
+	})
+
+	t.Run("Find the best fruit", func(t *testing.T) {
+		// arrange
+		best_fruit := "Banana"
+
+		fruits := []string{
+			"Apple",
+			"Banana",
+		}
+
+		// act
+
+		best, found := Find(fruits, func(s string) bool {
+			return strings.Contains(s, best_fruit)
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, best, best_fruit)
 	})
 }
