33e04b40d9182e26648509b09ad2d1c88d68f399
Johan Nilsson <jnsoft@users.noreply.github.com>
2025-06-07 10:05:21 +0000

    ok

---
+++ cache/cache_test.go
@@ -0,0 +1,13 @@
+package cache
+
+import (
+	"testing"
+
+	. "github.com/jnsoft/jngo/testhelper"
+)
+
+func TestBag(t *testing.T) {
+	t.Run("test", func(t *testing.T) {
+		AssertEqual(t, true, true)
+	})
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/jnsoft/jngo
 
-go 1.20
+go 1.24
